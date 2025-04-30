370cd6edd157e28a928425be6e27743fdfd46b11
Denis <79870237276@yandex.ru>
2025-05-01 00:38:27 +0500

    !

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -37,7 +37,7 @@ export class MainPageComponent {
 
   async ngOnInit() {
     this.dataStoreService.cityId$.pipe(
-      // distinctUntilChanged(),
+      distinctUntilChanged(),
       tap(cityId => console.log('check city', cityId)),
       filter(cityId => !!cityId),
       concatMap(() => this.wordpressIntegrationService.getHouses().pipe(
