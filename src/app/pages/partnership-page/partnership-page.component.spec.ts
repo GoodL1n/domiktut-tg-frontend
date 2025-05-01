10eace5b68d51b33d52d0af9cf3fcbbf39653b53
Denis <79870237276@yandex.ru>
2025-05-02 01:49:28 +0500

    + две страницы, пока пустые

---
+++ src/app/pages/partnership-page/partnership-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PartnershipPageComponent } from './partnership-page.component';
+
+describe('PartnershipPageComponent', () => {
+  let component: PartnershipPageComponent;
+  let fixture: ComponentFixture<PartnershipPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PartnershipPageComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PartnershipPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/partnership-page/partnership-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-partnership-page',
+  imports: [],
+  templateUrl: './partnership-page.component.html',
+  styleUrl: './partnership-page.component.scss'
+})
+export class PartnershipPageComponent {
+
+}
